512aeab0f14e2848818b70f83b967a90c728bbcd
chocopieo <ysj1106@gmail.com>
2021-07-26 15:27:44 +0900

    feignclient url 수정

--- src/main/java/ebookmanagement/external/PaymentService.java
+++ src/main/java/ebookmanagement/external/PaymentService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //@FeignClient(name="ebookmgmt-payment", url="http://localhost:8082")//, fallback = PaymentServiceFallback.class)
-@FeignClient(name="ebookmgmt-payment", url="http://ebookmgmt-payment:8080")//, fallback = PaymentServiceFallback.class)
+@FeignClient(name="user18-ebookmgmt-payment", url="http://user18-ebookmgmt-payment:8080")//, fallback = PaymentServiceFallback.class)
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")
