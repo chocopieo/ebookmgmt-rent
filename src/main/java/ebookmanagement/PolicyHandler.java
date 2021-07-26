4fb1a9cc1f8a22782272af8407e52f03ab0d4d3a
chocopieo <ysj1106@gmail.com>
2021-07-26 20:29:59 +0900

    policyhandler 수정

--- src/main/java/ebookmanagement/PolicyHandler.java
+++ src/main/java/ebookmanagement/PolicyHandler.java
@@ -12,24 +12,24 @@ import java.util.Date;
 public class PolicyHandler{
     @Autowired RentRepository rentRepository;
 
-    @StreamListener(KafkaProcessor.INPUT)
-    public void wheneverPaid_ChangeStatus(@Payload Paid paid){
-
-        if(!paid.validate()) return;
-
-        System.out.println("\n\n##### listener ChangeStatus : " + paid.toJson() + "\n\n");
-
-        Long rentId = paid.getRentId();
-        String status = paid.getStatus();
-
-        if("PAID".equals(status)) {
-            Rent rent = rentRepository.findById(rentId).get();
-            rent.setId(rentId);
-            rent.setStatus(status);
-
-            rentRepository.save(rent);
-        }
-    }
+//    @StreamListener(KafkaProcessor.INPUT)
+//    public void wheneverPaid_ChangeStatus(@Payload Paid paid){
+//
+//        if(!paid.validate()) return;
+//
+//        System.out.println("\n\n##### listener ChangeStatus : " + paid.toJson() + "\n\n");
+//
+//        Long rentId = paid.getRentId();
+//        String status = paid.getStatus();
+//
+//        if("PAID".equals(status)) {
+//            Rent rent = rentRepository.findById(rentId).get();
+//            rent.setId(rentId);
+//            rent.setStatus(status);
+//
+//            rentRepository.save(rent);
+//        }
+//    }
 
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverApproved_ChangeStatus(@Payload Approved approved){
